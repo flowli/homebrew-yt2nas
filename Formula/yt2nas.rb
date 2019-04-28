080f4466d45e5ef2f47c069d3cb7f0cb9395714f
Florian Arndt <post@florianarndt.com>
2019-04-28 12:19:50 +0200

    .

---
+++ Formula/yt2nas.rb
@@ -0,0 +1,10 @@
+class Yt2nas Formula
+  desc "Downloads at YT video and uploads it to the NAS"
+  homepage "https://arweb.de"
+  version "0.1"
+  url "https://github.com/flowli/homebrew-yt2nas/raw/master/yt2nas/yt2nas.zip", :using => :curl
+
+  def install
+     bin.install "yt2nas"
+  end
+end
